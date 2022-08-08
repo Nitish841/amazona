2a1e3af8773ad12d9af5b9af3d60349a0327b103
Nitish Kumar <nitishkqr841@gmail.com>
2022-08-08 15:54:02 +0530

    improve-add-to-cart

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,29 @@
-import express from "express";
-import data from "./data.js";
-import cors from "cors";
+import express from 'express';
+import data from './data.js';
+import cors from 'cors';
 
 const app = express();
 //const cors = require("cors");
 
 app.use(cors());
-app.get("/api/products", (req, res) => {
+app.get('/api/products', (req, res) => {
   res.send(data.products);
 });
-app.get("/api/products/slug/:slug", (req, res) => {
+app.get('/api/products/slug/:slug', (req, res) => {
   //console.log(req.params.slug);
   const product = data.products.find((x) => x.slug === req.params.slug);
   if (product) {
     res.send(product);
   } else {
-    res.status(404).send({ message: "Product Not Found" });
+    res.status(404).send({ message: 'Product Not Found' });
+  }
+});
+app.get('/api/products/:id', (req, res) => {
+  const product = data.products.find((x) => x._id === req.params.id);
+  if (product) {
+    res.send(product);
+  } else {
+    res.status(404).send({ message: 'Product Not Found' });
   }
 });
 
